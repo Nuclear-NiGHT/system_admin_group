8a3545e29355adbb01480b339af542a4f85e1453
18044816Dan <73891954+18044816Dan@users.noreply.github.com>
2021-01-01 15:17:36 +0000

    Add files via upload

--- Team1/GUI/GUIEmploy.py
+++ Team1/GUI/GUIEmploy.py
@@ -1,61 +1,81 @@
-import tkinter as tk
+from tkinter import *
 from PIL import Image, ImageTk
 from tkinter import messagebox
 
-root = tk.Tk()
+root = Tk()
 root.title("Login")
 
 
-def openMainPage():
-    windowLog = tk.Toplevel(root)
-    windowLog.title("Access Log")
-    windowLog.geometry("450x300")
-
+def default():
+    messagebox.showerror(message = "not implemented")
 
 def checkPassword():
     username = textName.get("1.0", 'end-1c')
     password = textPass.get("1.0", 'end-1c')
 
-    print(username)
-    print(password)
-
-    if username == "admin":
-        if password == "admin1":
+    if username == "1":
+        if password == "2":
             openMainPage()
-
-            
+ 
     else:
         messagebox.showerror(message = "Wrong Credentials")
         print("error")
 
+
+def openMainPage():
+
+    global canvas
+    global lavelName
+
+    windowLog = Toplevel()
+    windowLog.title("Access Log")
+
+    logCanvas = Canvas(root, width=100, height=150)
+    logCanvas.grid(columnspan=12)
+    windowLog.geometry('450x300')
+
+    logLabelName = Label(windowLog, text="testtest:", font = ("MyFont", 12))
+    logLabelName.grid(column=2, row=1, columnspan = 11)
+    logAdminButton = Button(windowLog, text = "Admin", font = "MyFont", command = default)
+    logAdminButton.grid(column = 1, row = 1)
+    logQuriesButton = Button(windowLog, text = "Quries", font = "MyFont", command = default)
+    logQuriesButton.grid(column = 1, row = 2)
+    logLoginsButton = Button(windowLog, text = "Log-ins", font = "MyFont", command = default)
+    logLoginsButton.grid(column = 1, row = 3)
+    logLogoutButton = Button(windowLog, text = "Log Out", font = "MyFont", command = default)
+    logLogoutButton.grid(column = 1, row = 4)
+    logQuitButtton = Button(windowLog, text = "Quit", font = "MyFont", command = default)
+    logQuitButtton.grid(column = 1, row = 5)
+
+
     
-canvas = tk.Canvas(root, width=100, height=150)
+canvas = Canvas(root, width=100, height=150)
 canvas.grid(columnspan=5)
 root.geometry('450x300')
 
 logo = Image.open('logo.gif')
 logo = ImageTk.PhotoImage(logo)
-logo_label = tk.Label(image=logo)
+logo_label = Label(image=logo)
 logo_label.image = logo
 logo_label.grid(column=2, row=0)
 
-labelTitle = tk.Label(root, text="Company Name", font = ("MyFont", 44))
+labelTitle = Label(root, text="Company Name", font = ("MyFont", 44))
 labelTitle.grid(column=3, row=0)
 
 
-labelName = tk.Label(root, text="Username:", font = ("MyFont", 12))
+labelName = Label(root, text="Username:", font = ("MyFont", 12))
 labelName.grid(column=2, row=1)
 
-textName = tk.Text(width = 30, height = 1)
+textName = Text(width = 30, height = 1)
 textName.grid(column = 3, row = 1)
 
-labelPass = tk.Label(root, text="Password:", font = ("MyFont", 12))
+labelPass = Label(root, text="Password:", font = ("MyFont", 12))
 labelPass.grid(column=2, row=2)
 
-textPass = tk.Text(width = 30, height = 1)
+textPass = Text(width = 30, height = 1)
 textPass.grid(column = 3, row = 2)
 
-btnLogin = tk.Button(root, text = "Log in", font = "MyFont", command = checkPassword)
+btnLogin = Button(root, text = "Log in", font = "MyFont", command = checkPassword)
 btnLogin.grid(column = 3, row = 3)
 
 
